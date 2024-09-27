7459bd1b1d53210f7d610e134cb55498c3140434
Ayobamiu <ayobamiu@gmail.com>
2024-09-27 12:19:56 -0400

    set trust proxy option to true

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ app.use(compression());
 app.use(cors());
 app.use(express.json({ limit: "10kb" }));
 
+app.set("trust proxy", 1);
+
 // Rate limiter
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
